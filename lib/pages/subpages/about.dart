2f7933555d8ac5b44b7c5fbb1b857ed53e1167ff
Cyrus Chan <cyruschan1212@gmail.com>
2021-03-31 17:49:15 +0800

    Link with google ready

--- lib/pages/subpages/about.dart
+++ lib/pages/subpages/about.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:one_take_pass_remake/pages/login.dart';
+import 'package:one_take_pass_remake/pages/reusable/link_google.dart';
 import 'package:one_take_pass_remake/themes.dart';
 
-class OTPAbout extends StatelessWidget {
+class OTPAbout extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _OTPAbout();
+}
+
+class _OTPAbout extends State<OTPAbout> {
+  _GoogleLink _gl = new _GoogleLink();
+  bool _status = true;
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Column(children: [
+      Padding(padding: EdgeInsets.only(top: 10)),
       Container(
         width: MediaQuery.of(context).size.width - 10,
         child: MaterialButton(
+          padding: EdgeInsets.all(10),
+          color: Colors.blue,
+          child: Text((_status ? "Unlink" : "Link") + " to Google",
+              style: TextStyle(fontSize: 16, color: Colors.white)),
+          onPressed: () {
+            if (!_status) {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => LinkGoogleNotify()));
+            }
+            _status = !_status;
+            setState(() {});
+          },
+        ),
+      ),
+      Padding(padding: EdgeInsets.only(top: 5)),
+      Container(
+        width: MediaQuery.of(context).size.width - 10,
+        child: MaterialButton(
+          padding: EdgeInsets.all(10),
           color: OTPColour.mainTheme,
           child: Text("Logout",
               style: TextStyle(fontSize: 16, color: Colors.white)),
@@ -43,5 +71,12 @@ class OTPAbout extends StatelessWidget {
         ),
       )
     ]));
+  }
+}
+
+class _GoogleLink {
+  bool _islinked = false;
+  bool get status {
+    return _islinked;
   }
 }
