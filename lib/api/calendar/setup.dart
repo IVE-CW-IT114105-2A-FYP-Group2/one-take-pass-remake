66091dd3e7c392323774cb5c4adbccbe0ac4eae0
Cyrus Chan <cyruschan1212@gmail.com>
2021-04-26 14:54:04 +0800

    Limit which object can be used

--- lib/api/calendar/setup.dart
+++ lib/api/calendar/setup.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import "package:googleapis_auth/auth_io.dart";
 import 'package:googleapis/calendar/v3.dart';
-import 'package:one_take_pass_remake/api/misc.dart';
+import 'package:one_take_pass_remake/api/misc.dart' show RegexLibraries;
 import 'package:url_launcher/url_launcher.dart';
 
 ///Initalize the google calendar apis
